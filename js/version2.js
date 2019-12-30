e58c540b8fd10a8bde6d1a86e4cdabba44cebd84
carlos <carlos@gmail.com>
2019-12-30 17:12:06 -0500

    dos versiones más agregadas

---
+++ js/version2.js
@@ -0,0 +1,14 @@
+"use strict";
+let floatTextMenuLinks = document.querySelectorAll(".float-text-menu li a");
+floatTextMenuLinks.forEach(link => {
+  let letters = link.textContent.split("");
+  link.textContent = "";
+  letters.forEach((letter, i) => {
+    let span = document.createElement("span");
+    span.textContent = letter;
+    span.style.transitionDelay = `${i / 10}s`;
+    span.dataset.text = letter;
+    link.append(span);
+  });
+});
+
